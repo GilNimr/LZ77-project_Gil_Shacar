5030afca4613d6939f7f75696ebe8d1da53b3df6
GilNimr <41791630+GilNimr@users.noreply.github.com>
2018-08-01 16:32:15 +0300

    Update Source.java

--- Source.java
+++ Source.java
@@ -4,16 +4,17 @@ public class Source {
 		public static void main(String[] args) {
 
 
-			String input_file = "C:\\Users\\shach\\Desktop\\Dhisat netunim2\\try.txt";
-			String compressed_file = "C:\\Users\\shach\\Desktop\\Dhisat netunim2\\outputtrial.txt";
-			String returned_file = "C:\\Users\\shach\\Desktop\\Dhisat netunim2\\outputtrial2.txt";
+			String input_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\hey hey my my.txt";
+			String compressed_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\outputtrial.txt";
+			String returned_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\outputtrial2.txt";
 			
-			String upgrade_side_file = "C:\\Users\\shach\\Desktop\\Dhisat netunim2\\upgrade_side_file.txt";
+			String upgrade_side_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\upgrade_side_file.txt";
 			
 			LZ77 lz = new LZ77();
-			//lz.Compress(input_file, compressed_file);
+			lz.Compress(input_file, compressed_file);
+			lz.Decompress(compressed_file, returned_file);
 			
-			lz.CompressWithUpgrade(input_file, compressed_file, upgrade_side_file);
+			//lz.CompressWithUpgrade(input_file, compressed_file, upgrade_side_file);
 			
 			System.out.println("Succeed");
 
@@ -21,5 +22,4 @@ public class Source {
 
 		}
 	}
-
 
