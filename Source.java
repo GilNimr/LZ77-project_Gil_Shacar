f4a5d8c31d8ab26a16a95a69a2ed5e23dc519f74
GilNimr <41791630+GilNimr@users.noreply.github.com>
2018-08-01 15:45:11 +0300

    Update Source.java

--- Source.java
+++ Source.java
@@ -17,11 +17,7 @@ public class Source {
 			
 			System.out.println("Succeed");
 
-			String input_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\OnTheOrigin.txt";
-			String compressed_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\outputtrial.txt";
-			String returned_file = "C:\\\\Users\\\\User\\\\Desktop\\\\Dhisat netunim2\\\\outputtrial2.txt";
-			LZ77 lz = new LZ77();
-			lz.Compress(input_file, compressed_file);
+			
 
 		}
 	}
